1232fd134e62aad63ab74e68b1885486feb1ace9
cbm2001 <90329802+cbm2001@users.noreply.github.com>
2021-10-08 00:42:21 +0400

    updated main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 console.log("hello, you just run Javascript from main.js!");
 const boardElement = document.getElementById("gameboard");
 console.log(boardElement);
+boardElement.innerHTML = "<p>Some new content for this div</p>";
+`<div class="col-3 board-cardflipped">
+  <div class="face-container">
+    <div class="facedown"></div>
+    <div class="faceup"></div>
+  </div>
+</div>`;
